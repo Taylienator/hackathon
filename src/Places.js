6c884dfdea2d7140882a54067815a951875a4b59
Taylor <taylorgreatlakes@yahoo.com>
2018-09-29 20:12:14 -0700

    added styling to data page

--- src/Places.js
+++ src/Places.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 export default props =>(
-    <div className='well'>
-    <h4>{props.name}</h4>
+
+    <div className='shadow row'>
+    <div class="col-lg-6">{props.name}</div>
     <img src={props.image} width='500' height='500'/>
     <a href={'https://maps.google.com/?q=' + props.latty + ',' + props.longy}
             className='btn btn-primary' target='value'> Directions </a>
